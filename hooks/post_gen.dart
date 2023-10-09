8a4762034f35b2c9e7dfb8eaba03bca0621e8ac7
Iván Martínez <ivanmart9503@gmail.com>
2023-10-09 08:05:58 -0600

    🏁 starting version for brick template

---
+++ hooks/post_gen.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+import 'package:mason/mason.dart';
+
+void run(HookContext context) {
+  final flutterVersion = context.vars['flutterVersion'] as String;
+
+  Process.run('fvm', ['use', flutterVersion]);
+
+  Process.run('fvm', ['flutter', 'pub', 'get']);
+
+  final progress = context.logger.progress('All done! 🎉');
+
+  progress.complete();
+}
